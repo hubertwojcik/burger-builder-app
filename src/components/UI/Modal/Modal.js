644c697140dadc56889e1b99e2644df34353d926
Hubert Wojcik <hwojcikk@gmail.com>
2020-04-13 22:45:18 +0200

    optimaze modal with React memo

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -21,4 +21,9 @@ const Modal = (props) => {
 	);
 };
 
-export default Modal;
+export default React.memo(
+	Modal,
+	(prevProps, nextProps) =>
+		nextProps.show == -prevProps.show &&
+		nextProps.children === prevProps.children
+);
