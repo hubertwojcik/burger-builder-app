53b17f25c049ddd3e0558f210bb90c0a02975464
Hubert Wojcik <hwojcikk@gmail.com>
2020-04-13 18:14:26 +0200

    use updateObject in Auth

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -6,6 +6,7 @@ import Button from "../../components/UI/Button/Button";
 import Spinner from "../../components/UI/spinner/Spinner";
 import classes from "./Auth.css";
 import * as actions from "../../store/actions/index";
+import { updateObject } from "../../shared/utility";
 
 class Auth extends Component {
 	state = {
@@ -77,17 +78,16 @@ class Auth extends Component {
 	}
 
 	inputChangedHandler = (event, controlName) => {
-		const updatedControl = {
-			...this.state.controls,
-			[controlName]: {
-				...this.state.controls[controlName],
+		const updatedControl = updateObject(this.state.controls, {
+			[controlName]: updateObject(this.state.controls[controlName], {
 				value: event.target.value,
 				valid: this.checkValidity(
 					event.target.value,
 					this.state.controls[controlName].validation
 				),
-			},
-		};
+			}),
+		});
+
 		this.setState({ controls: updatedControl });
 	};
 
