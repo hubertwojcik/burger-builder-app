395c3c371bd75294ecf93b9174d13a77faeeacf1
Hubert Wojcik <hwojcikk@gmail.com>
2020-04-13 16:51:10 +0200

    create layour container

---
+++ src/containers/layout/Layout.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import Aux from "../../hoc/Auxs";
+import classes from "./Layout.css";
+import Toolbar from "../../components/navigation/toolbar/Toolbar";
+import SideDrawer from "../../components/navigation/sidedrawer/SideDrawer";
+
+class Layout extends Component {
+	state = {
+		showSideDrawer: false,
+	};
+	sideDrawerClosedHandler = () => {
+		this.setState({ showSideDrawer: false });
+	};
+
+	sideDrawerToggleHandler = () => {
+		this.setState((prevState) => {
+			return { showSideDrawer: !prevState.showSideDrawer };
+		});
+	};
+
+	render() {
+		return (
+			<Aux>
+				<Toolbar
+					isAuth={this.props.isAuthenticated}
+					showDrawer={this.sideDrawerToggleHandler}
+				/>
+				<SideDrawer
+					isAuth={this.props.isAuthenticated}
+					closed={this.sideDrawerClosedHandler}
+					open={this.state.showSideDrawer}
+				/>
+				<main className={classes.Content}>{this.props.children}</main>
+			</Aux>
+		);
+	}
+}
+
+const mapStateToProps = (state) => {
+	return {
+		isAuthenticated: state.auth.token !== null,
+	};
+};
+
+export default connect(mapStateToProps)(Layout);
